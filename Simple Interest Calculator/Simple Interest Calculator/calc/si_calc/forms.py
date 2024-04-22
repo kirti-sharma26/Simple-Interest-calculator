9423ec344311a501ea092c29ff103b76999dbfcf
kirti-sharma26 <136141961+kirti-sharma26@users.noreply.github.com>
2024-04-22 18:03:30 +0530

    Add files via upload

---
+++ Simple Interest Calculator/Simple Interest Calculator/calc/si_calc/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Calculation
+
+class calculationForm(forms.ModelForm):
+    class Meta:
+        model = Calculation
+        fields = ['principal', 'rate', 'time']
